6bad6e103bc5bb50cd744a7bf52403a08ec7265d
Sara Rajabi <s.rajabi@Notrika.local>
2023-02-14 15:34:38 +0330

    initial commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='get_api',
       version='0.5',
       description='get_api by sara rajaei Demo Package',
-      url='#',
+      url='https://github.com/Sararajaei/get-api.git',
       author='Sara Rajaei',
       author_email='sararj9894@gmail.com',
       license='MIT',
